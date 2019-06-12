1afcdd8e32541f0ffcb4dc9e22da86c49991ff9e
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-12 23:54:28 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,6 +10,7 @@ Dir.entries(path).filter { |filename| filename.end_with?('.mp3')}
 end 
 
 def import 
-  files.each{|file| Song.new_by_filename
+  files.each{|file| Song.new_by_filename(file)
+end 
 
 end 
